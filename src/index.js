775630f84c9b7a15c12bbbf3c910ba538adfe1cc
Ilias Haddad <ilias.haddad@uit.ac.ma>
2022-04-19 15:47:48 +0000

    Add mongoose connection function and call fake users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+require('dotenv').config({});
+const mongoose = require('mongoose');
 const app = require('./app');
+const UserModel = require('./models/User');
+const { insertFakeUsers } = require('./utils');
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+app.listen(port, async () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
+  await mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ghiho.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+    },
+    async (err) => {
+      /* eslint-disable no-console */
+      console.log(err);
+      console.log('MongoDB connected');
+      /* eslint-enable no-console */
+
+      const users = await UserModel.countDocuments({});
+      if (users === 0) {
+        insertFakeUsers();
+      }
+    }
+  );
 });
