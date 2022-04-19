38175db2c2648f1e74eef94ff336c010cb3fb340
Ilias Haddad <ilias.haddad@uit.ac.ma>
2022-04-19 15:45:48 +0000

    Add upvotes API routes to handle comment upvotes

---
+++ src/api/upvote.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const CommentModel = require('../models/Comment');
+
+const router = express.Router();
+
+router.post('/:commentId', async (req, res) => {
+  try {
+    const { createdBy } = req.body;
+    const { commentId } = req.params;
+    const isExists = await CommentModel.findOne({
+      _id: commentId,
+      'upvotes.createdBy': createdBy,
+    });
+    if (isExists) {
+      const comment = await CommentModel.findOneAndUpdate(
+        { _id: commentId },
+        { $pull: { upvotes: { createdBy } } },
+        {
+          new: true,
+          runValidators: true,
+          context: 'query',
+        }
+      );
+      res.json(comment);
+    } else {
+      const comment = await CommentModel.findOneAndUpdate(
+        { _id: commentId },
+        { $push: { upvotes: { createdBy } } },
+        {
+          new: true,
+          runValidators: true,
+          context: 'query',
+        }
+      );
+      res.json(comment);
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+module.exports = router;
