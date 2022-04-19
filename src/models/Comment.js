b350dc47b4236d47c3ff2cef5dbf5abf6d830f5f
Ilias Haddad <ilias.haddad@uit.ac.ma>
2022-04-19 15:45:06 +0000

    Add comment model to store comments and upvotes

---
+++ src/models/Comment.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const commentSchema = new Schema({
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  content: {
+    type: String,
+    required: [true, 'Content required'],
+  },
+  upvotes: [
+    {
+      createdBy: String,
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+});
+
+const commentModel = model('Comment', commentSchema);
+
+module.exports = commentModel;
