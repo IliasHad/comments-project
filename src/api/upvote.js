cc08b69afde764f8467810aafa4a2a2287ef018b
Ilias Haddad <ilias.haddad@uit.ac.ma>
2022-04-22 14:07:05 +0000

    Send socket message when new upvote added or removed

--- src/api/upvote.js
+++ src/api/upvote.js
@@ -5,39 +5,42 @@ const router = express.Router();
 
 router.post('/:commentId', async (req, res) => {
   try {
-    const { createdBy } = req.body;
+    const { createdBy, parentCommentId } = req.body;
     const { commentId } = req.params;
     const isExists = await CommentModel.findOne({
       _id: commentId,
       'upvotes.createdBy': createdBy,
     });
+
     if (isExists) {
       const comment = await CommentModel.findOneAndUpdate(
         { _id: commentId },
         { $pull: { upvotes: { createdBy } } },
         {
-          new: true,
-          runValidators: true,
-          context: 'query',
+          new: true
         }
-      ).populate('createdBy').populate({
-        path: 'replies',
-        populate: { path: 'createdBy' }
-      });
+      )
+        .populate('createdBy')
+        .populate({
+          path: 'replies',
+          populate: { path: 'createdBy' },
+        });
+      res.io.sockets.emit('upvote', { comment, parentCommentId });
       res.json(comment);
     } else {
       const comment = await CommentModel.findOneAndUpdate(
         { _id: commentId },
         { $push: { upvotes: { createdBy } } },
         {
-          new: true,
-          runValidators: true,
-          context: 'query',
+          new: true
         }
-      ).populate('createdBy').populate({
-        path: 'replies',
-        populate: { path: 'createdBy' }
-      });
+      )
+        .populate('createdBy')
+        .populate({
+          path: 'replies',
+          populate: { path: 'createdBy' },
+        });
+      res.io.sockets.emit('upvote', { comment, parentCommentId });
       res.json(comment);
     }
   } catch (error) {
