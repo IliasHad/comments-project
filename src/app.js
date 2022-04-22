c04c3fadf630bfbb446c45e4c8f022743a2eb053
Ilias Haddad <ilias.haddad@uit.ac.ma>
2022-04-22 14:09:17 +0000

    Add socket io config with express server

--- src/app.js
+++ src/app.js
@@ -1,23 +1,35 @@
+require('dotenv').config();
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-
-require('dotenv').config();
-
 const middlewares = require('./middlewares');
 const api = require('./api');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_HOST
+  }
+});
 
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+io.on('connection', () => {
+  console.log('Socket Connected');
+});
 
+app.use((req, res, next) => {
+  res.io = io;
+  next();
+});
 app.use('/api/v1', api);
-
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-module.exports = app;
+module.exports = server;
