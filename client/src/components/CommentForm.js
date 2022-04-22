6aceb9c6e8bbc1bb0571118fdcaa4c47168a8da4
Ilias Haddad <ilias.haddad@uit.ac.ma>
2022-04-22 14:06:04 +0000

    Add loading state and reset form

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -1,12 +1,15 @@
 import { useForm } from "react-hook-form";
-
+import { useState } from "react";
 export const CommentForm = ({ user, setComments, comments }) => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
+  const [loading, setLoading] = useState(false);
   const onSubmit = (data) => {
+    setLoading(true);
     const userId = localStorage.getItem("userId");
     const { text } = data;
     fetch("/api/v1/comment", {
@@ -22,15 +25,17 @@ export const CommentForm = ({ user, setComments, comments }) => {
       .then((res) => res.json())
       .then((data) => {
         setComments([...comments, data]);
+        reset()
       })
       .catch((err) => {
         console.error(err);
-      });
+      })
+      .finally(() => setLoading(false));
   };
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      class="grid grid-columns-3 my-4 gap-4"
+      className="grid grid-columns-3 my-4 gap-4"
     >
       {errors.text && (
         <span className="text-sm text-red-500 mt-2 col-span-3">
@@ -40,7 +45,7 @@ export const CommentForm = ({ user, setComments, comments }) => {
       <div id="profile_image">
         {user && (
           <img
-            class="inline-block h-10 w-10 rounded-full"
+            className="inline-block h-10 w-10 rounded-full"
             src={user.profilePicture}
             alt={`${user.firstName} ${user.lastName}`}
           />
@@ -48,12 +53,12 @@ export const CommentForm = ({ user, setComments, comments }) => {
       </div>
 
       <label
-        for="text"
-        class="hidden sr-only text-sm font-medium text-gray-700"
+        htmlFor="text"
+        className="hidden sr-only text-sm font-medium text-gray-700"
       >
         Text
       </label>
-      <div class="mt-1 w-full">
+      <div className="mt-1 w-full">
         <input
           {...register("text", { required: true, minLength: 2 })}
           type="text"
@@ -77,7 +82,7 @@ export const CommentForm = ({ user, setComments, comments }) => {
       <button
         id="submit"
         type="submit"
-        class="
+        className="
                 inline-flex
                 items-center
                 px-2.5
@@ -96,28 +101,30 @@ export const CommentForm = ({ user, setComments, comments }) => {
                 focus:ring-indigo-500
               "
       >
-        <div id="loading" class="hidden">
-          <svg
-            class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              class="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              stroke-width="4"
-            ></circle>
-            <path
-              class="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        </div>
+        {loading && (
+          <div>
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        )}
         Comment
       </button>
     </form>
